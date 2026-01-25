e0e8b318ce14c54b8686522f02ff4ee70731137e
Sanc <sanchit.pahadi4u@gmail.com>
2026-01-25 18:03:32 +0000

    First triangle

---
+++ build/CMakeFiles/game_executable.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/game_executable.dir/glad.c.o"
+  "CMakeFiles/game_executable.dir/glad.c.o.d"
+  "CMakeFiles/game_executable.dir/main.cpp.o"
+  "CMakeFiles/game_executable.dir/main.cpp.o.d"
+  "game_executable"
+  "game_executable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/game_executable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
